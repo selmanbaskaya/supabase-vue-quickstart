cc53f4aab53b2b6fc13643a0d5b01fe4701304cb
Selman Baskaya <selmanbaskaya1@gmail.com>
2021-12-09 15:35:30 +0300

    initial commit

---
+++ src/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <form class="form-widget" @submit.prevent="updateProfile">
+    <!-- Add to body -->
+    <Avatar v-model:path="avatar_url" @upload="updateProfile" />
+
+    <!-- Other form elements -->
+  </form>
+</template>
+
+<script>
+  // Import the new component
+  import Avatar from './Avatar.vue'
+  
+  // 
+  components: {
+    Avatar,
+  }
+</script>
